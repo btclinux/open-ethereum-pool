1825629d1dcb1fbaa3fcdc390ed780fd57704b6b
btclinux <43704692+btclinux@users.noreply.github.com>
2020-07-17 16:49:48 +0300

    ethereum classic

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -34,7 +34,7 @@ const constantinopleHardForkHeight = 7280000
 
 var homesteadReward = math.MustParseBig256("5000000000000000000")
 var byzantiumReward = math.MustParseBig256("3000000000000000000")
-var constantinopleReward = math.MustParseBig256("2000000000000000000")
+var constantinopleReward = math.MustParseBig256("3200000000000000000")
 
 // Donate 10% from pool fees to developers
 const donationFee = 10.0
@@ -520,9 +520,7 @@ func getRewardForUncle(height int64) *big.Int {
 
 func getUncleReward(uHeight, height int64) *big.Int {
 	reward := getConstReward(height)
-	k := height - uHeight
-	reward.Mul(big.NewInt(8-k), reward)
-	reward.Div(reward, big.NewInt(8))
+	reward.Div(reward, big.NewInt(32))
 	return reward
 }
 
